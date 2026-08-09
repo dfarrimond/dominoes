ad1fd8b176eec4bd0cdcab1c75c79350f4d7cd4b
Dan Farrimond <dfarrimond@me.com>
2026-08-09 13:20:30 +0100

    Refresh PWA cache for branching doubles

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='little-dominoes-v2';
+const CACHE='little-dominoes-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/apple-touch-icon.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-512-maskable.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
