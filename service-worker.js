2d8bd5330a90a4d761e0ecdcfa46eecc77fd442f
Dan Farrimond <dfarrimond@me.com>
2026-08-09 18:09:11 +0100

    Refresh PWA cache for multiplayer

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,34 @@
-const CACHE='little-dominoes-v6';
-const ASSETS=['./','./launcher.html','./index.html','./manifest.webmanifest','./icons/apple-touch-icon.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-512-maskable.png'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);const scopePath=new URL(self.registration.scope).pathname;if(event.request.mode==='navigate'&&url.origin===location.origin&&url.pathname===scopePath){event.respondWith(caches.match('./launcher.html').then(cached=>cached||fetch('./launcher.html')));return;}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>event.request.mode==='navigate'?caches.match('./launcher.html'):caches.match('./index.html'))));});
+const CACHE='little-dominoes-v7';
+const ASSETS=[
+  './',
+  './launcher.html',
+  './index.html',
+  './online.html',
+  './manifest.webmanifest',
+  './icons/apple-touch-icon.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png'
+];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  const scopePath=new URL(self.registration.scope).pathname;
+  if(event.request.mode==='navigate'&&url.origin===location.origin&&url.pathname===scopePath){
+    event.respondWith(caches.match('./launcher.html').then(cached=>cached||fetch('./launcher.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>event.request.mode==='navigate'?caches.match('./launcher.html'):caches.match('./index.html'))));
+});
